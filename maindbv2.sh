053934bff31b5228d09917262cfa74177cd16809
techy2dev <73438596+techy2dev@users.noreply.github.com>
2026-01-28 11:30:49 +0600

    maindbv2.sh

--- maindbv2.sh
+++ maindbv2.sh
@@ -709,10 +709,7 @@ install_done()
   echo
   echo
   history -c;
-  rm /root/.installer
-  echo "Server will secure this server and reboot after 20 seconds"
-  sleep 20
-  #reboot
+  rm maindbv2.sh
 }
 
 server_interface=$(ip route get 8.8.8.8 | awk '/dev/ {f=NR} f&&NR-1==f' RS=" ")
